ea09b6ec1604c145bf079cb5414447d2cd7838a7
saltfile <3205617363@qq.com>
2023-06-06 21:54:29 +0800

    06-06

--- TimelyDB_V0.1.2/time_pool/t_pool.h
+++ TimelyDB_V0.1.2/time_pool/t_pool.h
@@ -30,95 +30,12 @@ using namespace std;
 
 
 
-/**
- * push() 在队尾插入一个元素
-
-pop() 删除队列第一个元素
-
-size() 返回队列中元素个数
-
-empty() 如果队列空则返回true
-
-front() 返回队列中的第一个元素
-
-back() 返回队列中最后一个元素
- */
-template<typename T>
-class bloking_queue{
-private:
-    queue<T> task_queue;
-    pthread_mutex_t lock;
-public:
-    bloking_queue();
-
-    bool is_empty();
-
-    int size();
-
-    void push(T data);
-
-    void pop();
-
-    T front();
-
-};
 
 
-template<typename T>
-bloking_queue<T>::bloking_queue() {
-    if (pthread_mutex_init(&lock,NULL) != 0){
-        perror("queue lock init fail");
-    }
-}
-
-template<typename T>
-bool bloking_queue<T>::is_empty(){
-    bool res = true;
-    pthread_mutex_lock(&this->lock);
-    res = !this->task_queue.empty();
-    pthread_mutex_unlock(&this->lock);
-    return res;
-}
 
 
-template<typename T>
-int bloking_queue<T>:: size(){
-    int res;
-    pthread_mutex_lock(&this->lock);
-    res = this->task_queue.size();
-    pthread_mutex_unlock(&this->lock);
-    return res;
 
-}
-
-template<typename T>
-void bloking_queue<T>::push(T data){
-
-    pthread_mutex_lock(&this->lock);
-    this->task_queue.push(data);
-    pthread_mutex_unlock(&this->lock);
-
-}
-
-
-template<typename T>
-void bloking_queue<T>::pop(){
-    pthread_mutex_lock(&this->lock);
-    this->task_queue.pop();
-    pthread_mutex_unlock(&this->lock);
-
-}
-
-template<typename T>
-T bloking_queue<T>::front(){
-    T res;
-    pthread_mutex_lock(&this->lock);
-    res = this->task_queue.front();
-    this->task_queue.pop();
-    pthread_mutex_unlock(&this->lock);
-
-    return res;
-}
+//TODO:block_queue重做 c撰写内核链表
 
 
 
