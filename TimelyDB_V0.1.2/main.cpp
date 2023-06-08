b99b42edeb2dcb504531d592f78d0a61a4c4dc76
saltfile <3205617363@qq.com>
2023-06-08 23:00:25 +0800

    06-08

--- TimelyDB_V0.1.2/main.cpp
+++ TimelyDB_V0.1.2/main.cpp
@@ -12,17 +12,25 @@
 
 
 
-
 //
 int main() {
 
+    tylist_node *tyus = (tylist_node*) malloc(sizeof (tylist_node)) ;
 
+    tylist_node *tyus1 = (tylist_node*) malloc(sizeof (tylist_node));
+    tylist_node *tyus2 = (tylist_node*) malloc(sizeof (tylist_node));
+    tylist_node *tyus3 = (tylist_node*) malloc(sizeof (tylist_node));
 
+    tyus->next = tyus1;
+    tyus1->next = tyus2;
+    tyus2->next = tyus3;
+    tylist_node *pos = NULL;
 
+    LIST_LAST(pos,tyus);
 
-
-
-
-
+        tylist_node *tyus4 = (tylist_node*) malloc(sizeof (tylist_node));
+    tylist_node *p = NULL;
+    LIST_ADD_TAIL(tyus,p,tyus4)
+    LIST_LAST(pos,tyus);
     return 1;
 }
--- TimelyDB_V0.1.2/time_pool/t_pool.h
+++ TimelyDB_V0.1.2/time_pool/t_pool.h
@@ -24,15 +24,52 @@
 #include <random>
 #include <iostream>
 #include <queue>
+
 using namespace std;
 #ifndef TIMELYDB_V0_1_2_T_POOL_H
 #define TIMELYDB_V0_1_2_T_POOL_H
 
+#define LIST_LAST(pos, head) \
+    for (pos =(head) ;pos->next ;pos = pos->next)
+
+#define LIST_FOREACH(pos, head) \
+    for (pos =(head) ;pos->next ;pos = pos->next)
+
+#define LIST_ADD_TAIL(head, pos, node) \
+    for (pos =(head) ;pos->next ;pos = pos->next);            \
+    pos->next = node;                  \
+    node->prev = pos;                  \
 
 
 
+typedef struct tylist_node {
+    tylist_node *prev;
+    tylist_node *next;
+} tylist_node;
+
+template<typename T>
+class tylink_list {
+private:
+    struct tylist_node list;
+
+    T data;
+    int length = -1;
+    int aim = -1;
+public:
+    void init_node(T data) {
+        this->data = data;
+    }
+
+    void set_data(T data) {
+        this->data = data;
+    }
+
+    T get_data() {
+        return this->data;
+    }
 
 
+};
 
 
 //TODO:block_queue重做 c撰写内核链表
