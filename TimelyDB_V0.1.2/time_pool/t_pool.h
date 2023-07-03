0d536ce2b9d067c5ded337dc98bd63819c0dd428
saltfile <3205617363@qq.com>
2023-07-03 22:45:06 +0800

    07-03

--- TimelyDB_V0.1.2/time_pool/t_pool.h
+++ TimelyDB_V0.1.2/time_pool/t_pool.h
@@ -53,7 +53,7 @@ typedef struct tylist_node {
     tylist_node *prev;
     tylist_node *next;
 } tylist_node;
-
+//QUEUE
 template<typename T>
 class tylink_list {
 private:
@@ -80,6 +80,105 @@ public:
 
 
 //TODO:block_queue重做 c撰写内核链表
+
+
+
+
+class task{
+public:
+    void* arg;
+    void (*func)(void *arg);//被执行函数指针
+};
+
+class thread_pool{
+public:
+    vector<task> vector;
+    int currt;//当前任务量
+    int capacity;//队列容量
+    int queue_head;//队列头   获取数据
+    int queue_tail;//队列尾   放进数据
+
+    pthread_t admin;//管理员线程
+    pthread_t *works;//工作线程
+
+    int max_size;//工作线程组最大容量
+    int min_size;//工作线程组最小容量
+    int working_num;//正在工作的线程数量
+    int live_num;//存活的线程数量
+    int kill_num;//杀死线程的个数
+
+    pthread_mutex_t pool_lock;//线程池的互斥锁
+    pthread_mutex_t working_lock;//给正在工作的线程数加一层锁保护
+
+    pthread_cond_t full_vbe;//任务队列满
+    pthread_cond_t empty_vbe;//任务队列空
+
+    int poweroff;//销毁线程池 0开 1关
+
+   thread_pool(int work_num,int min,int queue_size){
+       do {
+           this->currt = 0;
+           if (work_num < 1)break;
+           if (queue_size < 2)break;
+           if (min > work_num||min < 0)break;
+           this->max_size = work_num;
+           this->min_size = min;
+           this->live_num = min;
+           this->working_num = 0;
+           this->kill_num = 0;
+
+           this->capacity = queue_size;
+           this->queue_head = 0;
+           this->queue_tail = 0;
+
+           if (pthread_mutex_init(&this->pool_lock, NULL) != 0 ||
+               pthread_mutex_init(&this->working_lock, NULL) != 0 ||
+               pthread_cond_init(&this->empty_vbe, NULL) != 0 ||
+               pthread_cond_init(&this->full_vbe, NULL) != 0) {
+
+               perror("初始化锁失败");
+               break;
+           }
+
+
+
+
+           pthread_create(&this->admin, NULL, reinterpret_cast<void *(*)(void *)>(contoller_p), this);
+           for (int i = 0; i < min; ++i) {
+               pthread_create(&this->works[i], NULL, reinterpret_cast<void *(*)(void *)>(work), pool);
+           }
+
+
+
+
+
+           this->poweroff = 1;
+
+           cout<<"任务池初始化成功"<<endl;
+
+           return;
+       } while (0);
+
+       this->poweroff = 0;
+
+
+   }
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
 
