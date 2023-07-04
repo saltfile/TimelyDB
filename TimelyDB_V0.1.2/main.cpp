1b239e2c8650c684c537b34996deef4ee143221c
saltfile <3205617363@qq.com>
2023-07-04 23:16:39 +0800

    07-04还是有问题，哎...力不从心啊

--- TimelyDB_V0.1.2/main.cpp
+++ TimelyDB_V0.1.2/main.cpp
@@ -3,7 +3,10 @@
 
 
 
-
+void fun(void* a){
+    int* b = (int*)a;
+    cout<<"执行任务"<<&b<<endl;
+}
 
 
 
@@ -15,32 +18,21 @@
 //
 int main() {
 
-    tylist_node *tyus = (tylist_node*) malloc(sizeof (tylist_node)) ;
-//
-//    tylist_node *tyus1 = (tylist_node*) malloc(sizeof (tylist_node));
-//    tylist_node *tyus2 = (tylist_node*) malloc(sizeof (tylist_node));
-//    tylist_node *tyus3 = (tylist_node*) malloc(sizeof (tylist_node));
-//
-//    tyus->next = tyus1;
-//    tyus1->next = tyus2;
-//    tyus2->next = tyus3;
-//    tylist_node *pos = NULL;
-//
-//    LIST_LAST(pos,tyus);
-//
-//        tylist_node *tyus4 = (tylist_node*) malloc(sizeof (tylist_node));
-//    tylist_node *p = NULL;
-//    LIST_ADD_TAIL(tyus,p,tyus4)
-//    LIST_LAST(pos,tyus);
-tylink_list<int> *a =new tylink_list<int>(2);
-    tylist_node *p = NULL;
-    tylist_node *o = &a->list;
-    LIST_ADD_TAIL(tyus,p,o);
-//((class duty_node*)((char *)(p)-(unsigned long )(&((duty_node *)0)->list))); head type pos
-//    ((typeof(tylink_list<int>) *)((char *)(p)-(unsigned long )(&((typeof(tylink_list<int>)*)0)->list)));
-p = tyus->next;
-tylink_list<int> *b = CONTAINER_OF(b,tylink_list<int>,p);
-cout<<b->get_data();
+    thread_pool *pool = new thread_pool(10,3,100);
+
+    for (int i = 0; i < 100; ++i) {
+        void* arg = &i;
+        pool->pool_add_task(fun, (void *)arg);
+        sleep(1);
+    }
+
+    sleep(10);
+
+
+
+
+
+
 
     return 1;
 }
--- TimelyDB_V0.1.2/time_pool/t_pool.h
+++ TimelyDB_V0.1.2/time_pool/t_pool.h
@@ -24,7 +24,6 @@
 #include <random>
 #include <iostream>
 #include <queue>
-
 using namespace std;
 #ifndef TIMELYDB_V0_1_2_T_POOL_H
 #define TIMELYDB_V0_1_2_T_POOL_H
@@ -43,6 +42,12 @@ using namespace std;
 
 #define CONTAINER_OF(head,type,pos) \
    head = ((typeof(type) *)((char *)(pos)-(unsigned long )(&((typeof(type)*)0)->list)))
+
+
+#define NUMBRE 2
+void* contoller_p(void* arg);
+void* work(void* arg);
+
 
 
 
@@ -84,6 +89,31 @@ public:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class task{
 public:
     void* arg;
@@ -92,7 +122,7 @@ public:
 
 class thread_pool{
 public:
-    vector<task> vector;
+    vector<task> tasks;
     int currt;//当前任务量
     int capacity;//队列容量
     int queue_head;//队列头   获取数据
@@ -131,6 +161,13 @@ public:
            this->queue_head = 0;
            this->queue_tail = 0;
 
+           this->works = (pthread_t *) malloc(sizeof(pthread_t) * work_num);
+           memset(this->works, 0, sizeof(pthread_t) * work_num);
+           if (this->works == NULL) {
+               perror("工作线程组创建失败");
+               break;
+           }
+
            if (pthread_mutex_init(&this->pool_lock, NULL) != 0 ||
                pthread_mutex_init(&this->working_lock, NULL) != 0 ||
                pthread_cond_init(&this->empty_vbe, NULL) != 0 ||
@@ -140,17 +177,10 @@ public:
                break;
            }
 
-
-
-
-//           pthread_create(&this->admin, NULL, reinterpret_cast<void *(*)(void *)>(contoller_p), this);
-//           for (int i = 0; i < min; ++i) {
-//               pthread_create(&this->works[i], NULL, reinterpret_cast<void *(*)(void *)>(work), pool);
-//           }
-
-
-
-
+           pthread_create(&this->admin, NULL, reinterpret_cast<void *(*)(void *)>(contoller_p), this);
+           for (int i = 0; i < min; ++i) {
+               pthread_create(&this->works[i], NULL, reinterpret_cast<void *(*)(void *)>(work), this);
+           }
 
            this->poweroff = 1;
 
@@ -161,26 +191,160 @@ public:
 
        this->poweroff = 0;
 
-
    }
+void pool_add_task(void (*func)(void *), void *arg) {
+        pthread_mutex_lock(&this->pool_lock);
+        cout<<"我添加了"<<endl;
+
+
+        while (this->currt == this->capacity && !this->poweroff) {
+            pthread_cond_wait(&this->full_vbe, &this->pool_lock);
+        }
+
+
+        if (this->poweroff) {
+            pthread_mutex_unlock(&this->pool_lock);
+        }
+
+
+        //添加任务
+      //TODO:这里需要大改一波将vector使用熟练
+
+        pthread_cond_signal(&this->empty_vbe);
+
+
+        pthread_mutex_unlock(&this->pool_lock);
+
+
+
+    }
 
 
 
 
 };
 
+void pool_exit(thread_pool *pool);
+
+void* contoller_p(void* arg){
+    thread_pool *pool = (thread_pool*)arg;
+
+    while (pool->poweroff != 0){
+        sleep(5);
+        //取出线程池中任务数量和当前线程数量
+        pthread_mutex_lock(&pool->pool_lock);
+        int present = pool->currt;
+        int live = pool->live_num;
+        pthread_mutex_unlock(&pool->pool_lock);
+
+        //取出正在忙的线程数量
+        pthread_mutex_lock(&pool->working_lock);
+        int busy = pool->working_num;
+        pthread_mutex_unlock(&pool->working_lock);
+
+
+        //添加线程
+        //当当前任务个数大于存活线程个数并且存活线程数小于最大线程数
+        if (present > live && live < pool->max_size){
+            int count = 0;
+            for(int i = 0 ;i < pool->max_size&&count < 2&&pool->live_num < pool->max_size;i++){
+                if (pool->works[i] == 0){
+                    pthread_create(&pool->works[i],NULL,work,pool);
+                    count++;
+                    pool->live_num++;
+                }
+
+            }
+        }
+
+
+        //销毁线程
+        //忙的线程*2小于存活的线程并且存活的线程小于最小线程数
+        if (busy*2 < live&&live < pool->min_size){
+
+            pthread_mutex_lock(&pool->pool_lock);
+            pool->kill_num = NUMBRE;
+            pthread_mutex_unlock(&pool->pool_lock);
+            //杀死线程
+            for (int i = 0; i < NUMBRE; ++i) {
+                pthread_cond_signal(&pool->empty_vbe);
+            }
+        }
+
+    }
+}
+
+
+
+
+void* work(void* arg){
+    thread_pool* pool = (thread_pool*)arg;
+    while (1){
+
+
+
+        pthread_mutex_lock(&pool->pool_lock);
+
+        //当前任务队列如果为空并且线程池没有关闭
+        while (pool->currt == 0&& !pool->poweroff){
+            //阻塞工作线程
+            pthread_cond_wait(&pool->empty_vbe,&pool->pool_lock);
+
+            if (pool->live_num > 0){
+                pool->kill_num--;
+                if (pool->live_num > pool->min_size){
+                    pool->live_num--;
+                    pthread_mutex_unlock(&pool->pool_lock);
+                    pool_exit(pool);
+                }
+            }
+        }
+
+        //判断线程池是否被关闭
+        if (pool->poweroff){
+            pthread_mutex_unlock(&pool->pool_lock);
+            pool_exit(pool);
+        }
+
+        //开始消费线程
+        task task;
+        task.func = pool->tasks[pool->queue_head].func;
+        task.arg = pool->tasks[pool->queue_head].arg;
+        //循环队列->如果对其中的长度进行取余那么运行到最后一个的时候+1取余刚好为0，就刚好回到队列的开头
+        pool->queue_head = (pool->queue_head+1)%pool->capacity;
+        pool->currt--;
+        pthread_cond_signal(&pool->full_vbe);
+        pthread_mutex_unlock(&pool->pool_lock);
+        //正在忙的线程需要处理
+        pool->working_num++;
+        pthread_mutex_lock(&pool->working_lock);
+        task.func(task.arg);
+        task.arg = NULL;
+
+        pthread_mutex_unlock(&pool->working_lock);
+        pool->working_num--;
+        pthread_mutex_unlock(&pool->working_lock);
+
+
+    }
+
+
+}
 
 
 
 
 
-
-
-
-
-
-
-
+void pool_exit(thread_pool *pool){
+    pthread_t tid = pthread_self();
+    for (int i = 0; i < pool->max_size; ++i) {
+        if (pool->works[i] == tid){
+            pool->works[i] = 0;
+            cout<<"线程"<<tid<<"退出"<<endl;
+        }
+    }
+    pthread_exit(NULL);
+}
 
 
 
