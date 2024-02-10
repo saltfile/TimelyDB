1c2b169f63f7d23db7ec94d242221cb878fa9a5d
saltfile <3205617363@qq.com>
2024-02-10 22:52:48 +0800

    01-22 规定内存环形链表状态

--- TimelyDB_V0.1.2/engine/eng_base.h
+++ TimelyDB_V0.1.2/engine/eng_base.h
@@ -4,5 +4,75 @@
 
 #ifndef TIMELYDB_V0_1_2_ENG_BASE_H
 #define TIMELYDB_V0_1_2_ENG_BASE_H
+#include <error.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <cstring>
+#include <iostream>
+#include <string.h>
+#include <cstring>
+#include <unistd.h>
+#include <pthread.h>
+#include <sched.h>
+#include <typeinfo>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <future>
+#include <cstdlib>
+#include <exception>
+#include "../tool/tool_base.h"
+using namespace std;
+
+enum data_type{
+    BYTE=0,
+    INT=1,
+    FLOAT=2,
+    DOUBLE=3,
+    DECIMAL=4,
+    VARCHAR=5,
+    TEXT=5,
+    TIMESTAMP=8
+};
+//早期做法
+typedef struct DB_table{
+    int clonms_size;
+    char **clonms;
+    char *tab_name;
+    char *base_name;
+    int val_size;
+    int val_max;
+    arr_list *val_;//数据节点
+};
+//最懒省事做法
+typedef struct tab_struct{
+    int max_data_size;
+    map<char*,data_type> type_map;
+    map<char*,arr_list*> data_map;
+};
+
+
+//创建表
+bool DB_create_table(char* base_name,char* tab_name,char** clonms,data_type * types,int clonms_size);
+
+
+
+
+
+
+
+
+
+
 
 #endif //TIMELYDB_V0_1_2_ENG_BASE_H
--- TimelyDB_V0.1.2/engine/storage.cpp
+++ TimelyDB_V0.1.2/engine/storage.cpp
@@ -1,3 +1,58 @@
 //
 // Created by saltfish on 23-4-19.
 //
+#include "eng_base.h"
+
+static map<string, map<string, tab_struct>> DB_TAB_MAP;
+
+
+
+
+
+
+
+
+/**
+ * 建表的映射
+ * 表结构文件存储方式：
+ *  clonm_name$类型序号;
+ */
+
+bool DB_create_table(char* base_name,char* tab_name,char** clonms,data_type * types,int clonms_size){
+    int is_succ = create_table(base_name,tab_name);
+    if(is_succ != 1)return false;
+
+    //TODO：明天要将这里写完
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
